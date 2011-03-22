ac89748094c5990fb07708058251025075b24373
Filip de Waard <fmw@vix.io>
2011-03-22 03:41:59 +0100

    added initial files created by Leinigen

---
+++ src/lucene_filter_problem_demo/core.clj
@@ -0,0 +1,1 @@
+(ns lucene-filter-problem-demo.core)
---
+++ test/lucene_filter_problem_demo/test/core.clj
@@ -0,0 +1,6 @@
+(ns lucene-filter-problem-demo.test.core
+  (:use [lucene-filter-problem-demo.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
