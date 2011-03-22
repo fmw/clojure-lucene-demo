d7b8735a6b9968b414105f769aa7f205fc96997e
Filip de Waard <fmw@vix.io>
2011-03-22 06:39:43 +0100

    added generic demonstration of broken filters

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject lucene-filter-problem-demo "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clojure-couchdb "0.4.5"]
+                 [org.apache.lucene/lucene-core "3.0.3"]
+                 [org.apache.lucene/lucene-queries "3.0.3"]]
+  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]]
+  :main lucene-filter-problem-demo.core)
+
